3338912f5b1477c0a1d89fdb33cb31713c29a922
AdenChen27 <adenchen2005@gmail.com>
2025-08-03 20:08:47 -0500

    Downgrade to Python 3.12 for Nix compatibility

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 resume:
-	rendercv render resume.yaml -nomd -nohtml -nopng --use-local-latex-command "pdflatex"
+	python -m rendercv render resume.yaml -nomd -nohtml -nopng --use-local-latex-command "pdflatex"
 	cp rendercv_output/Aden_Chen_CV.pdf resume.pdf
 	open resume.pdf
--- flake.nix
+++ flake.nix
@@ -1,7 +1,38 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.python3
-    pkgs.poetry
-  ];
+{
+  description = "RenderCV resume builder";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, poetry2nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        p2n = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+
+        pythonEnv = p2n.mkPoetryEnv {
+          projectDir = ./.;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pythonEnv
+            pkgs.texliveFull
+            pkgs.git
+          ];
+
+          # Optional: helpful env vars
+          shellHook = ''
+            echo "Python + RenderCV dev shell activated"
+          '';
+        };
+      });
 }
+
