70b5ef25f0c2f01a03792b94a015683f6328c723
AdenChen27 <adenchen2005@gmail.com>
2025-08-03 20:12:25 -0500

    Fix flake with poetry2nix and Python 3.12

--- flake.nix
+++ flake.nix
@@ -1,38 +1,37 @@
 {
-  description = "RenderCV resume builder";
+  description = "rendercv resume project with poetry2nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix.url = "github:nix-community/poetry2nix";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, poetry2nix, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        p2n = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
-
-        pythonEnv = p2n.mkPoetryEnv {
-          projectDir = ./.;
-        };
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pythonEnv
-            pkgs.texliveFull
-            pkgs.git
-          ];
-
-          # Optional: helpful env vars
-          shellHook = ''
-            echo "Python + RenderCV dev shell activated"
-          '';
-        };
-      });
+  outputs = { self, nixpkgs, poetry2nix, ... }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+      forAllSystems = f:
+        builtins.listToAttrs (map (system: {
+          name = system;
+          value = f system;
+        }) systems);
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ poetry2nix.overlays.default ];
+          };
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              # Makes `rendercv` CLI available in the shell
+              (pkgs.poetry2nix.mkPoetryApplication {
+                projectDir = ./.;
+                python = pkgs.python312;
+              })
+            ];
+          };
+        });
+    };
 }
 
